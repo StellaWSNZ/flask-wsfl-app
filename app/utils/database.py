9acaa3f2b5c66388521f9c1f374b43cc9f5ccf2a
StellaWSNZ <Stella@watersafety.org.nz>
2025-05-28 13:48:17 +1200

    IP should not change

--- app/utils/database.py
+++ app/utils/database.py
@@ -11,6 +11,5 @@ def get_db_engine():
 
     return create_engine(
         f"mssql+pyodbc://{os.getenv('WSNZDBUSER')}:{os.getenv('WSNZDBPASS')}"
-        "@heimatau.database.windows.net:1433/WSFL?driver=ODBC+Driver+18+for+SQL+Server",
-        fast_executemany=True
+        "@heimatau.database.windows.net:1433/WSFL?driver=ODBC+Driver+18+for+SQL+Server"
     )
