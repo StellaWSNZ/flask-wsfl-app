bf9e747c9e1b2e7afc3675deab7ae072670795b5
StellaWSNZ <Stella@watersafety.org.nz>
2026-03-10 16:56:28 +1300

    all clm funder staff  to see approved providers

--- app/routes/apr.py
+++ app/routes/apr.py
@@ -270,7 +270,7 @@ def apr_page():
         external_statuses=external_statuses,
         database_statuses=database_statuses,
         self_statuses=self_statuses,
-        can_edit = (session.get("user_role")=="ADM"),
+        can_edit = (session.get("user_role")=="ADM" or session.get("user_id")==17),
         APPROVED_SET_TODAY_ID=APPROVED_SET_TODAY_ID,
         LESSON_SET_TODAY_ID=LESSON_SET_TODAY_ID,
         EXTERNAL_SET_TODAY_ID=EXTERNAL_SET_TODAY_ID,
